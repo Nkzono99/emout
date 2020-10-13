8d4aaf1145cef1b167e3100e2be90c7ebae28ce3
Nkzono99 <1735112t@gsuite.kobe-u.ac.jp>
2020-10-13 18:33:37 +0900

    Create: setup.pyを追加しpipでインストールできるようにした

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='emout',
+    description='Emses output manager',
+    version='0.1.0',
+    install_requires=['numpy', 'h5py'],
+    author='Nkzono99',
+    author_email='1735112t@gsuite.stu.kobe-u.ac.jp',
+    url='https://github.com/Nkzono99/emout',
+    packages=find_packages()
+)
