a834b02b4d7aa74d013538d2ab8fd52d6258e4d3
Nkzono99 <1735112t@gsuite.kobe-u.ac.jp>
2020-10-20 22:00:04 +0900

    Update: emout.plotで公開する関数を変更

--- emout/plot/__init__.py
+++ emout/plot/__init__.py
@@ -1,2 +1,1 @@
-from emout.plot.basic_plot import plot_2dmap, plot_line
-from emout.plot.plot import plot_xline, plot_yline, plot_zline, plot_xy, plot_xz, plot_yz
+from emout.plot.basic_plot import *
