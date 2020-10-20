2c5160331aca94988e08c15e4ed33ed852207f96
Nkzono99 <1735112t@gsuite.kobe-u.ac.jp>
2020-10-20 19:39:17 +0900

    Update: 基本的プロット処理関数にdocstringを追加

--- emout/plot/basic_plot.py
+++ emout/plot/basic_plot.py
@@ -15,6 +15,35 @@ def plot_2dmap(data2d,
                title=None,
                interpolation='bilinear',
                dpi=10):
+    """2次元カラーマップをプロットする.
+
+    Parameters
+    ----------
+    data2d : numpy.ndarray
+        2次元データ
+    mesh : (numpy.ndarray, numpy.ndarray), optional
+        メッシュ, by default None
+    savefilename : str, optional
+        保存するファイル名(Noneの場合保存しない), by default None
+    cmap : matplotlib.Colormap or str or None, optional
+        カラーマップ, by default cm.coolwarm
+    vmin : float, optional
+        最小値, by default None
+    vmax : float, optional
+        最大値, by default None
+    figsize : (float, float), optional
+        図のサイズ, by default None
+    xlabel : str, optional
+        x軸のラベル, by default None
+    ylabel : str, optional
+        y軸のラベル, by default None
+    title : str, optional
+        タイトル, by default None
+    interpolation : str, optional
+        用いる補間方法, by default 'bilinear'
+    dpi : int, optional
+        解像度(figsizeが指定された場合は無視される), by default 10
+    """
     if savefilename is not None:
         if figsize is None:
             px = 1/plt.rcParams['figure.dpi'] * dpi
@@ -59,6 +88,31 @@ def plot_line(data1d,
               ylabel=None,
               label=None,
               title=None):
+    """1次元データをプロットする.
+
+    Parameters
+    ----------
+    data1d : array-like or scalar
+        プロットする1次元データ
+    x : array-like or scalar
+        横軸となる1次元データ, by default None
+    savefilename : str, optional
+        保存するファイル名, by default None
+    vmin : float, optional
+        最小値, by default None
+    vmax : float, optional
+        最大値, by default None
+    figsize : (float, float), optional
+        図のサイズ, by default None
+    xlabel : str, optional
+        横軸のラベル, by default None
+    ylabel : str, optional
+        縦軸のラベル, by default None
+    label : str, optional
+        ラベル, by default None
+    title : str, optional
+        タイトル, by default None
+    """
     if savefilename is not None:
         fig = plt.figure(figsize=figsize)
 
