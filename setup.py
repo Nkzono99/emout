46b989aba28b29ed50511ae3b5221fe1786ed91c
Nkzono99 <1735112t@gsuite.kobe-u.ac.jp>
2020-10-21 05:34:49 +0900

    Update: setup.pyのバージョン更新

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def _require_packages(filename):
 setup(
     name='emout',
     description='Emses output manager',
-    version='0.4.0',
+    version='0.4.2',
     install_requires=_require_packages('requirements.txt'),
     author='Nkzono99',
     author_email='1735112t@gsuite.stu.kobe-u.ac.jp',
