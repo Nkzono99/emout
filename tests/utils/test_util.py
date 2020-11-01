afaff395410db199a984cee82066294f02e60b39
Nkzono99 <1735112t@gsuite.kobe-u.ac.jp>
2020-11-01 23:01:40 +0900

    Add: テストケースの追加

--- tests/utils/test_util.py
+++ tests/utils/test_util.py
@@ -7,7 +7,19 @@ def test_regexdict():
         r'a[0-9]+': 5,
         r'b[0-9]': 10,
     })
+    assert 'a' in name2unit
+    assert 'a1' in name2unit
+    assert 'b1' in name2unit
+    assert 'b8' in name2unit
+    assert 'abc' not in name2unit
+    assert 'b88' not in name2unit
+
     assert name2unit['a'] == 0
     assert name2unit['a1'] == 5
     assert name2unit['b1'] == 10
     assert name2unit['b8'] == 10
+
+    assert name2unit.get('a') == 0
+    assert name2unit.get('a1') == 5
+    assert name2unit.get('b1') == 10
+    assert name2unit.get('b8') == 10
