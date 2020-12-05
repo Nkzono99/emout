2a1dd5c80177467d6e29b8bd46900982f1a6fd83
Nkzono99 <1735112t@gsuite.kobe-u.ac.jp>
2020-12-06 05:18:46 +0900

    Update: setup.pyのバージョンを更新

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='Emses output manager',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version='0.9.2',
+    version='0.10.0',
     install_requires=_require_packages('requirements.txt'),
     author='Nkzono99',
     author_email='1735112t@gsuite.stu.kobe-u.ac.jp',
